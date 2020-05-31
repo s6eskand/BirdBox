f8522e1e354aeb91ebf92d0281bf45e3088b0aaf
Sam <sam.eskandar00@hotmail.com>
2020-05-31 16:56:00 -0400

    add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { HashRouter as Router, Route, Switch} from 'react-router-dom';
+import CreateAccount from "./Components/CreateAccount";
+import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+          <Switch>
+              <Route exact path="/register" component={CreateAccount} />
+          </Switch>
+      </Router>
     </div>
   );
 }
