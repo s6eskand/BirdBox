23c50006a0f73a86640e84d856b793dd02bcdcba
Sam <sam.eskandar00@hotmail.com>
2020-05-31 21:58:06 -0400

    update routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,23 @@ import React from 'react';
 import './App.css';
 import { HashRouter as Router, Route, Switch} from 'react-router-dom';
 import CreateAccount from "./Components/CreateAccount";
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css/semantic.min.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import Login from './Components/Login';
 
 function App() {
   return (
-    <div>
-      <Router>
-          <Switch>
-              <Route exact path="/register" component={CreateAccount} />
-          </Switch>
-      </Router>
-    </div>
+      <Provider store={store}>
+        <div>
+          <Router>
+              <Switch>
+                  <Route exact path="/register" component={CreateAccount} />
+                  <Route exact path="/login" component={Login} />
+              </Switch>
+          </Router>
+        </div>
+      </Provider>
   );
 }
 
