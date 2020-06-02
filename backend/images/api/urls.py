e2d24b7231c1da840f7d45c6b1c981328c688d89
Sam <sam.eskandar00@hotmail.com>
2020-06-01 21:42:52 -0400

    create url endpoint for bird images

--- backend/images/api/urls.py
+++ backend/images/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from .views import ImageViewSet
 
 urlpatterns = [
-    path('api/images/', ImageViewSet.as_view)
+    path('api/images/', ImageViewSet.as_view())
 ]
