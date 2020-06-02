f9ec1da1fee457b4095ff436644c36154e46ee6a
Sam <sam.eskandar00@hotmail.com>
2020-06-01 21:43:15 -0400

    add API view for bird images

--- backend/images/api/views.py
+++ backend/images/api/views.py
@@ -1,15 +1,16 @@
 from ..models import Image
 from rest_framework import viewsets, permissions
 from .serializers import ImageSerializer
-from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView
 
 
-class ImageViewSet(viewsets.ModelViewSet):
+class ImageViewSet(ListCreateAPIView):
+
+    # just allow any for now until I set up redux and private routes on client side
+    # if you cant retrieve data from here/are getting 403 Errors, ensure CORS is enabled on whichever server youre using
     permission_classes = [
-        permissions.IsAuthenticated
+        permissions.AllowAny
     ]
 
+    queryset = Image.objects.all()
     serializer_class = ImageSerializer
-
-    def get(self):
-        return self.request.user.images.all()
