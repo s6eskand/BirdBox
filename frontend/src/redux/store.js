d5adbead46d95899cdae6273a415caa6e689037c
Sam <sam.eskandar00@hotmail.com>
2020-06-16 09:51:21 -0400

    update redux store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,18 +1,17 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import rootReducer from './reducers';
+import rootReducer from "./reducers";
 
-const initialState = {
-
-};
 
 const middleware = [
     thunk,
 ];
 
+const storeMonitor = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
     rootReducer,
-    initialState,
+    storeMonitor(applyMiddleware(...middleware))
 );
 
 export default store;
