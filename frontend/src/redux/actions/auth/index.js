bda284a94591ee2578e9a33875211654a41b23b0
Sam <sam.eskandar00@hotmail.com>
2020-06-17 00:05:21 -0400

    update redux auth actions

--- frontend/src/redux/actions/auth/index.js
+++ frontend/src/redux/actions/auth/index.js
@@ -1,88 +1,23 @@
 import {
-    AUTH_FAIL,
     AUTH_LOGOUT,
-    AUTH_START,
-    AUTH_SUCCESS,
+    AUTH_LOGIN,
+    AUTH_REGISTER,
 } from "../../constants/auth";
-import axios from 'axios';
 
-const handleResponse = (res) => {
-    const token = res.data.key;
-    const expirationDate = new Date(new Date().getTime() + 3600 * 1000);
+export const login = (token) => ({
+    type: AUTH_LOGIN,
+    token: token,
+    isAuthenticated: true
+});
 
-    localStorage.setItem('token', token);
-    localStorage.setItem('expirationDate', expirationDate);
+export const logout = () => ({
+    type: AUTH_LOGOUT,
+    token: null,
+    isAuthenticated: false
+});
 
-    dispatch(authSuccess(token));
-    dispatch(checkAuthTimeout(3600));
-};
-
-export const authStart = () => {
-    return {
-        type: AUTH_START
-    }
-};
-
-export const authSuccess = (token) => {
-    return {
-        type: AUTH_SUCCESS,
-        token: token
-    }
-};
-
-export const authFail = (error) => {
-    return {
-        type: AUTH_FAIL,
-        error: error
-    }
-};
-
-export const checkAuthTimeout = (expirationTime) => {
-    return dispatch => {
-        setTimeout(() => {
-            dispatch(logout())
-        }, expirationTime * 1000)
-    }
-};
-
-export const logout = () => {
-    localStorage.removeItem('user');
-    localStorage.removeItem('expirationDate');
-    return {
-        type: AUTH_START
-    }
-};
-
-export const authLogin = (username, email, password) => {
-    return dispatch => {
-        dispatch(authStart());
-        axios.post('http://127.0.0.1:8000/api/auth/login', {
-            username: username,
-            email: email,
-            password: password
-        })
-            .then(res => {
-                handleResponse(res)
-            })
-            .catch(err => {
-                dispatch(authFail(err))
-            })
-    }
-};
-
-export const authRegister = (username, email, password) => {
-    return dispatch => {
-        dispatch(authStart());
-        axios.post('http://127.0.0.1:8000/api/auth/register', {
-            username: username,
-            email: email,
-            password: password
-        })
-            .then(res => {
-                handleResponse(res)
-            })
-            .catch(err => {
-                dispatch(authFail(err))
-            })
-    }
-};
+export const register = async (token) =>({
+    type: AUTH_REGISTER,
+    token: token,
+    isAuthenticated: true
+});
