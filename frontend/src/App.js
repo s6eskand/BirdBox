21f80ef829f24faa60d9b1294273773a4cc43103
Sam <sam.eskandar00@hotmail.com>
2020-06-16 23:59:47 -0400

    add PersistGate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import CreateAccount from "./Components/CreateAccount";
+import CreateAccount from "./components/auth/CreateAccount";
 import 'semantic-ui-css/semantic.min.css';
 import { Provider } from 'react-redux';
-import store from './redux/store';
-import Login from './Components/Login';
-import Birds from "./Views/Birds";
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import Login from './components/auth/Login';
+import Birds from "./views/Birds";
+import PersistLoader from "./components/loader/Loader";
 
 function App() {
   return (
       <Provider store={store}>
-        <div>
-          <Router>
-              <Switch>
-                  <Route exact path="/register" component={CreateAccount} />
-                  <Route exact path="/login" component={Login} />
-                  <Route exact path="/" component={Birds} />
-              </Switch>
-          </Router>
-        </div>
+          {/*TODO add loading component*/}
+          <PersistGate loading={<PersistLoader/>} persistor={persistor}>
+            <div>
+              <Router>
+                  <Switch>
+                      <Route exact path="/register" component={CreateAccount} />
+                      <Route exact path="/login" component={Login} />
+                      <Route exact path="/" component={Birds} />
+                  </Switch>
+              </Router>
+            </div>
+          </PersistGate>
       </Provider>
   );
 }
