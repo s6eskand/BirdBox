274d431d3d93bc67158d4c99e6ff518438fdf685
Sam <sam.eskandar00@hotmail.com>
2020-06-17 00:01:46 -0400

    move api endpoints into constants dir

---
+++ frontend/src/constants/endpoints/index.js
@@ -0,0 +1,5 @@
+export const LOGIN_ENDPOINT = 'http://localhost:8000/api/auth/login';
+export const LOGOUT_ENDPOINT = 'http://localhost:8000/api/auth/logout';
+export const REGISTER_ENDPOINT = 'http://localhost:8000/api/auth/register';
+export const GET_USER_ENDPOINT = 'http://localhost:8000/api/auth/user';
+export const GET_AND_POST_IMAGES = 'http://localhost:8000/api/images';
